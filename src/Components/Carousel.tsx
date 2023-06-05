aeb273918f68cff6850aefbab64cd7c9cff7288e
HudsonGraeme <me@hudsongrae.me>
2023-06-05 00:05:36 -0400

    Stop trying to reinvent the wheel

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,43 +1,49 @@
-import { Box, Flex, HStack, Text, useColorModeValue, VStack } from '@hope-ui/solid'
+import { Box, Flex, HStack, Text, useColorModeValue } from '@hope-ui/solid'
 import { useNavigate } from '@solidjs/router'
 import isEmpty from 'lodash/isEmpty'
 import { FiExternalLink } from 'solid-icons/fi'
-import { createSignal, For, Show } from 'solid-js'
+import { For, Show } from 'solid-js'
+import { Swiper, SwiperSlide } from 'swiper/solid'
+import 'swiper/swiper-bundle.css'
 import { BASE_URL_PLACEHOLDER } from '../utilities/constants'
 import Button from './Button'
-import Image from './Image'
+
 const Carousel = ({ posts }) => {
-  const [currentImage, setCurrentImage] = createSignal(0)
   const hoverColor = useColorModeValue('$whiteAlpha11', '$blackAlpha11')
   const navigate = useNavigate()
-  const nextImage = () => {
-    if (currentImage() + 1 < posts().length) {
-      return currentImage() + 1
-    }
-    return 0
-  }
-
-  const prevImage = () => {
-    if (currentImage() - 1 >= 0) {
-      return currentImage() - 1
-    }
-    return posts().length - 1
-  }
 
   return (
     <Show when={!isEmpty(posts())}>
-      <VStack spacing="$4" alignItems="center" maxW="90%">
-        <Box pos="relative" overflow="hidden" h="$sm" w="$full" minW="$sm">
-          <HStack pos="absolute" spacing="$4" h="$full" w="$full" transform={`translateX(-${currentImage() * 100}%yd)`}>
-            <For each={posts()}>
-              {({ slug, attributes: { title, image } }) => (
-                <Flex pos="relative" role="group">
-                  <Image src={image.replace(BASE_URL_PLACEHOLDER, import.meta.env.VITE_BASE_IMG_URL)} />
+      <Flex
+        w="$sm"
+        minH="$xs"
+        bg="#edf3f8"
+        _dark={{
+          bg: '#3e3e3e',
+        }}
+        p={10}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Swiper style={{ width: '100%', flex: 1, height: '100%' }}>
+          <For each={posts()}>
+            {({ slug, attributes: { title, image } }) => (
+              <SwiperSlide>
+                <Box
+                  boxSize="full"
+                  shadow="md"
+                  w="$full"
+                  h="$full"
+                  background={`url(${image.replace(BASE_URL_PLACEHOLDER, import.meta.env.VITE_BASE_IMG_URL)})`}
+                  style={{ 'background-size': 'cover', 'background-repeat': 'no-repeat' }}
+                  role="group"
+                  alignItems="end"
+                  justifyItems="center"
+                  display="flex"
+                >
                   <HStack
-                    position="absolute"
-                    bottom={0}
-                    left={0}
-                    right={0}
+                    w="$full"
+                    position="relative"
                     p="$2"
                     transition="opacity 0.2s linear"
                     opacity={0}
@@ -52,20 +58,12 @@ const Carousel = ({ posts }) => {
                       View Post
                     </Button>
                   </HStack>
-                </Flex>
-              )}
-            </For>
-          </HStack>
-        </Box>
-        <HStack spacing="$4">
-          <Button variant="ghost" onClick={() => setCurrentImage(prevImage())}>
-            Prev
-          </Button>
-          <Button variant="ghost" onClick={() => setCurrentImage(nextImage())}>
-            Next
-          </Button>
-        </HStack>
-      </VStack>
+                </Box>
+              </SwiperSlide>
+            )}
+          </For>
+        </Swiper>
+      </Flex>
     </Show>
   )
 }
